66e8c154ec191ebe4fc478115d9d2341ecbef977
Jose Sagastume <jose.sagast@gmail.com>
2025-11-17 17:11:03 -0700

    Rolls and player 1 results 1-6

--- app.js
+++ app.js
@@ -73,23 +73,86 @@ const rollDice=()=>{
             };
         };
     }else{
-        console.log('no rolls left for this round');
+        //show this as message
+        console.log('no rolls left for this round');// here goes the switch.
         return;
     };
     player.rolls +=1;
     updateDisplay();
     rollsLeftEl.innerText=gameState.rollsCount-=1;
+    upperSection();
 };
+
+const upperSection=()=>{
+    const player=gameState.players[gameState.currentPlayerIndex];
+    let tally = player.diceValue.reduce((acc,dieValue)=>{
+        if(acc[dieValue]){
+            acc[dieValue] = acc[dieValue] + 1;
+          } else {
+            acc[dieValue] = 1;
+          }
+          return acc;
+        }, {});
+    
+    const scoreOpts = ['ones', 'twos', 'threes', 'fours', 'fives', 'sixes'];
+
+    for (let face = 1; face <7; face++) {
+        const option = scoreOpts[face - 1]; // index = face - 1 to access elements in array 
+        player.scores[option]= tally[face]? tally[face]*face:0;
+    };
+
+    updateDisplay();
+};
+
+// const threeOfKind=()={ // same functions will be created for 4
+//     //will check value of dice comparing value and adding it's value if they meet the criteria and add it to score cards
+// };
+
+// const fullHouse=()={/
+//     // will check value of dice and determine if full criteria is met
+// };
+
+// const straight=()={
+//     // will explore if large or small straights were achieved and adding 40 to the score card cell that corresponds
+// };
+
+// const chance=()=>{
+    
+// }
+
+// const checkFinalScore=()=>{
+//     //will look through the state to find who won (highest score) when rounds=maxRounds
+//     //will display if player one or two was the winner (call the updateMessage function)
+// };
+
+
+
+
+
 
 const updateDisplay = () => {
     const player = gameState.players[gameState.currentPlayerIndex];
-  
+    const scoreCells = document.querySelectorAll(`td.p${gameState.currentPlayerIndex + 1}`);
+
+
     player.diceValue.forEach((dieValue, index) => {
       const dieEl = document.querySelector(`#die-${index}`);// to select the right die based on index
       dieEl.classList.remove('face-1','face-2','face-3','face-4','face-5','face-6');// remove current face  
       dieEl.classList.add(`face-${dieValue}`); //add face after rolling
     });
+
+    scoreCells.forEach(cell => {//iterating through all td p1 elements- query selector returns node list
+        const category = cell.dataset.typeScore; // each category defined in my html
+        if (player.scores[category] !== null) {        // only show scores that exist [] because has to be dynamic
+          cell.textContent = player.scores[category];
+        } else {
+          cell.textContent = '';                // empty if score not yet assigned
+        }
+      });
+
 };
+
+
 
 const init=()=>{
     gameState.players=[
